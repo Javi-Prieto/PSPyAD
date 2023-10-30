593936b72df17572675c07621b93b06d953ebc54
Javier Prieto <jpojaviprior@gmail.com>
2023-10-30 23:05:56 +0100

    Added example for configuration

---
+++ ExampleForconfiguration/src/main/java/com/example/ExampleForconfiguration/ExampleForconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExampleForconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleForconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleForconfigurationApplication.class, args);
+	}
+
+}
---
+++ ExampleForconfiguration/src/test/java/com/example/ExampleForconfiguration/ExampleForconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExampleForconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleForconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
