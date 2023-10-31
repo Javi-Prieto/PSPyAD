940f31144120b256812ea177359b2c92b3267c65
Javier Prieto <jpojaviprior@gmail.com>
2023-10-31 12:56:59 +0100

    Initial commit for this example

---
+++ exampleForDBConnection/src/test/java/com/example/exampleForDBConnection/ExampleForDbConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleForDBConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleForDbConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
