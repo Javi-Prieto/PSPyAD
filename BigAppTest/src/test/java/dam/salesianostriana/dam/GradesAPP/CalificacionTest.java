2883f2a951b1227193e072418796446535e3dafe
Javier Prieto <jpojaviprior@gmail.com>
2024-01-18 15:55:59 +0100

    almost finalized i cannot completly make the tryouts to the service due to the fact that i do not understand mockito at all and i cannot create to exact equal instances of the same object

--- BigAppTest/src/test/java/dam/salesianostriana/dam/GradesAPP/CalificacionTest.java
+++ BigAppTest/src/test/java/dam/salesianostriana/dam/GradesAPP/CalificacionTest.java
@@ -4,6 +4,7 @@ import dam.salesianostriana.dam.GradesAPP.alumno.model.Alumno;
 import dam.salesianostriana.dam.GradesAPP.alumno.repository.AlumnoRepository;
 import dam.salesianostriana.dam.GradesAPP.asignatura.model.Asignatura;
 import dam.salesianostriana.dam.GradesAPP.asignatura.repository.AsignaturaRepository;
+import dam.salesianostriana.dam.GradesAPP.calificacion.DTO.GETCalificacionDTO;
 import dam.salesianostriana.dam.GradesAPP.calificacion.DTO.POSTCalificacionDTO;
 import dam.salesianostriana.dam.GradesAPP.calificacion.model.Calificacion;
 import dam.salesianostriana.dam.GradesAPP.calificacion.repository.CalificacionRepository;
@@ -47,8 +48,10 @@ public class CalificacionTest {
 
     public static Stream<Arguments> getData() {
         return Stream.of(
-                Arguments.arguments("1", POSTCalificacionDTO.builder().calificacion(9.37).codRef("1").idAlumno("1").build()),
-                Arguments.arguments("0", POSTCalificacionDTO.builder().calificacion(9.37).codRef("1").idAlumno("0").build())
+                Arguments.arguments(UUID.fromString("430c3bda-9d92-497f-ad1f-cb3231f29671"), POSTCalificacionDTO.builder().calificacion(9.37).codRef("1").idAlumno("09b69d6f-2991-420c-a2fe-63555858b499").build()),
+                Arguments.arguments(UUID.fromString("89316b95-15ca-4111-a85c-8b239e9dfbae"), POSTCalificacionDTO.builder().calificacion(9.37).codRef("1a").idAlumno("842c2849-d95b-4f63-9451-337a9b544578").build()),
+                Arguments.arguments(UUID.fromString("363e122a-be2b-4b38-958b-106cfe0f4eb1"), POSTCalificacionDTO.builder().calificacion(9.37).codRef("2").idAlumno("0d26eac2-cbad-4e3a-8c69-9731c8bbe9fb").build())
+
         );
     }
 
@@ -61,7 +64,7 @@ public class CalificacionTest {
                 .referentes(Set.of())
                 .asignatura(asignatura)
                 .contenidos("Hola")
-                .id(UUID.fromString("1"))
+                .id(UUID.fromString("f1d585bd-2dcc-49bb-b104-eb40150cff6c"))
                 .build();
         Optional<Alumno> selectedAl = Optional.of(Alumno.builder()
                 .asignaturas(Set.of(asignatura))
@@ -71,15 +74,22 @@ public class CalificacionTest {
                 .build());
 
         List<ReferenteEvaluacion> referentes = List.of(ReferenteEvaluacion.builder().codReferente("1").asignatura(asignatura).descripcion("desc").build());
-        if (id.toString().equals("1")) {
+        ReferenteEvaluacion ref = referentes.get(0);
+        Calificacion toProve = POSTCalificacionDTO.from(selectedAl.get(), newCal, referentes.get(0), selected);
+        if (id.toString().equals("430c3bda-9d92-497f-ad1f-cb3231f29671")) {
             Mockito.when(repoIns.existsById(id)).thenReturn(true);
             Mockito.when(repoIns.getReferenceById(id)).thenReturn(selected);
             Mockito.when(repoAl.findById(UUID.fromString(newCal.idAlumno()))).thenReturn(selectedAl);
             Mockito.when(repoAs.getAllReferentes()).thenReturn(referentes);
             if(newCal.codRef().equals("1")){
-
+                Mockito.when(repo.save(POSTCalificacionDTO.from(selectedAl.get(),newCal, ref, selected))).thenReturn(toProve);
+                GETCalificacionDTO result = service.createCalificacion(id,newCal);
+                assertEquals(GETCalificacionDTO.of(toProve), result);
             }else{
-
+                Throwable throwable = assertThrows(NotFoundException.class, () ->{
+                    service.createCalificacion(id, newCal);
+                });
+                assertEquals("The Referente or the list could not be found",throwable.getMessage());
             }
         }
         else {
